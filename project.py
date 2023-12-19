c91d5c6c596964e2aa0fe18266e18323d854ca92
tyanochku_by <ramyud@st.amu.edu.pl>
2023-12-19 10:36:54 +0100

    best k selection added, readme added

--- project.py
+++ project.py
@@ -2,19 +2,17 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 import pandas as pd
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
+from sklearn.metrics import silhouette_score
 import matplotlib.pyplot as plt
-import numpy as np
-from scipy import sparse
+from time import time
+
+J = lambda X, labels: silhouette_score(X, labels, metric='euclidean', sample_size=None, random_state=None)
 
 
-k = 4
 
-df = pd.read_csv(r'J:\hoojnia\py\PJN\projekt\Emails.csv')
-# df['text'] = df['text'].apply(
-#     lambda x: x.split(' ', 1)[1]
-# )
+df = pd.read_csv(r'D:\hoojnia\py\PJN\projekt\nlp-tdidf-kmeans\Emails.csv')
+
 emails = df['text'].to_numpy()
-print(emails[0])
 tfidf = TfidfVectorizer(
     ngram_range=(1,3),
     stop_words = 'english',
@@ -22,16 +20,38 @@ tfidf = TfidfVectorizer(
 )
 tfidf_matrix = tfidf.fit_transform(emails)
 
-clustering_model = KMeans(n_clusters=k, random_state=0, n_init="auto")
+best_k = 2
+best_j = 100000
+best_clustering_model = KMeans(n_clusters=1, random_state=0, n_init="auto")
 
-labels = clustering_model.fit_predict(tfidf_matrix)
+print("k\t\ttime\tsilhouette")
+formatter_result = ("{:9s}\t{:.3f}s\t{:.3f}")
 
-kilka = 5
-for i in range(k):
-    print('Kilka maili {} kategorii:\n'.format(i+1))
+for k in range(2, 10):
+    next_kmeans = KMeans(n_clusters=k, random_state=0, n_init="auto")
+
+    t0 = time()
+    next_kmeans.fit_predict(tfidf_matrix)
+    fit_time = time() - t0
+    
+    j = J(tfidf_matrix, next_kmeans.labels_)
+    print(formatter_result.format(str(k), fit_time, j))
+
+    if j < best_j:
+        best_j = j
+        best_k = k
+        best_clustering_model = next_kmeans
+
+print(f'\nBest k: {best_k}\n\n\n')
+
+kilka = 4
+best_clustering_model.fit_predict(tfidf_matrix)
+
+for i in range(best_k):
+    print(f'Kilka maili {i+1} kategorii:\n')
     j = 0
-    for label_n in range(len(clustering_model.labels_)):
-        if clustering_model.labels_[label_n] == i:
+    for label_n in range(len(best_clustering_model.labels_)):
+        if best_clustering_model.labels_[label_n] == i:
             print(emails[label_n])
             print('\n\n')
             j+=1
@@ -42,6 +62,6 @@ for i in range(k):
 x = tfidf_matrix.toarray()
 reduced_data = PCA(n_components=2).fit_transform(x)
 fig, ax = plt.subplots()
-plt.scatter(reduced_data[:,0], reduced_data[:,1], c=clustering_model.labels_)
+plt.scatter(reduced_data[:,0], reduced_data[:,1], c=best_clustering_model.labels_)
 
 plt.show()
---
+++ readme.txt
@@ -0,0 +1,9 @@
+Ten program stosuje metodę uczenia nienadzorowanego k-średnich i macierz tdidf. 
+Aby dobrać jak najlepszą liczbę k, do oceniania używam silhouette_score. 
+Wypisywane są kilka maili z każdej grupy.
+Używam również PCA do redukcji wymiarów i wizualizacji danych.
+
+Python version==3.12.0
+matplotlib==3.8.1
+pandas==2.1.4
+scikit_learn==1.3.2
